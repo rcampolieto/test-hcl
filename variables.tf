df8d66bf29174f1bd36287dcd97678e3a1d20328
campiFoo <rcampolieto@users.noreply.github.com>
2020-01-15 20:50:38 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,5 +14,5 @@ variable "tags" {
 
 variable "resource_group_name" {
  description = "The name of the resource group in which the resources will be created"
- default     = "hcorppybuild01"
+ default     = "hcorppybuild001"
 }
